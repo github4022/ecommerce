03611c7e268777ffe9234f68e67ecc1e1440777c
Prasad Sharma <prasdsharma@project.com>
2024-05-10 11:40:13 +0530

    Integrate djnago-bootstrap and rendered home page

--- core/settings/base.py
+++ core/settings/base.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Third Party Apps
     'debug_toolbar',  # debug-toolbar app
+    'bootstrap5',
     # Local Apps
     'accounts',
     'pages',
